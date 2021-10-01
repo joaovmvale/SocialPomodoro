0801a4c8b10db26e8f3b06dfad980a5ff058d199
joaovmvale <joao.marquesvale@gmail.com>
2021-09-30 23:58:22 -0300

    [30/09/2021] - Inital commit, developing authentication and registration

---
+++ src/components/Login/style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const Styles = StyleSheet.create({
+  titulo: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+  input: {
+    borderWidth: 1,
+    width: 300,
+    padding: 5,
+    marginBottom: 5,
+    backgroundColor: "white",
+  },
+});
+
+export default Styles;
