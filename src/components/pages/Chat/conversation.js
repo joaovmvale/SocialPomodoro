7c0f9260c42e07e5cd526e4ae453fb927dc968c3
joaovmvale <joao.marquesvale@gmail.com>
2021-11-07 22:11:32 -0300

    Hora funzional

--- src/components/pages/Chat/conversation.js
+++ src/components/pages/Chat/conversation.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useContext } from "react";
 import { View, Text, Alert, StyleSheet, Dimensions, FlatList, KeyboardAvoidingView } from "react-native";
 import { Button, TextInput } from "react-native-paper";
 
+import axios from "axios";
 import firebase from "../../utils/Firebase";
 import AuthContext from "../../contexts/auth";
 import { Ionicons } from "@expo/vector-icons";
@@ -15,7 +16,7 @@ export default function Conversation() {
   const [messages, setMessages] = useState([])
   const { user, currentChat } = useContext(AuthContext);
 
-  useEffect(()=>{
+  useEffect(() => {
 
     loadMessages()
 
@@ -24,19 +25,24 @@ export default function Conversation() {
 
   }, [])
 
-  function sendMessage(e){
+  async function sendMessage(e) {
 
     e.stopPropagation()
 
     let docRef = firebase.firestore().collection('Conversations').doc(currentChat).
-    collection('Messages').doc()
+      collection('Messages').doc()
     let docID = docRef.id
+    let currenTime;
+    await axios.get("http://worldtimeapi.org/api/timezone/America/Sao_Paulo").then((res) => {
+      currenTime = res.data.unixtime;
+    })
 
+    console.log(currenTime);
 
     let messageObject = {
-    
+
       id: docID,
-      createdTime: firebase.firestore.Timestamp.now(),
+      createdTime: new Date(currenTime*1000),
       message: newMessage,
       userID: user.uid
 
@@ -46,41 +52,41 @@ export default function Conversation() {
 
     setNewMessage('')
 
-  } 
+  }
 
-  async function loadMessages(){
+  async function loadMessages() {
 
     firebase.firestore().collection('Conversations').doc(currentChat).
-    collection('Messages').onSnapshot(messagesSnapshot=>{
+      collection('Messages').onSnapshot(messagesSnapshot => {
 
-      let list = []
-      messagesSnapshot.forEach(message=>{
+        let list = []
+        messagesSnapshot.forEach(message => {
 
-        let messageTimeSeconds = message.data().createdTime.seconds
-        let messageOBJ = {...message.data(), messageTimeSeconds}
-        messageOBJ.createdTime = new Date(messageTimeSeconds * 1000)
+          let messageTimeSeconds = message.data().createdTime.seconds
+          let messageOBJ = { ...message.data(), messageTimeSeconds }
+          messageOBJ.createdTime = new Date(messageTimeSeconds * 1000)
 
-        list.unshift(messageOBJ)  
-        list.sort((a, b)=>{return a.messageTimeSeconds - b.messageTimeSeconds})
-        setMessages([...list])
+          list.unshift(messageOBJ)
+          list.sort((a, b) => { return a.messageTimeSeconds - b.messageTimeSeconds })
+          setMessages([...list])
+
+        })
 
       })
-
-    })
   }
 
   return (
-    <KeyboardAvoidingView 
-    style={{flex: 1}}
-    behavior={Platform.OS === "ios" ? "padding" : "height"}
-     keyboardVerticalOffset={130}
-     enabled
-     >
-      
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={130}
+      enabled
+    >
+
       <View style={styles.messages}>
 
         <FlatList
-         
+
           showsVerticalScrollIndicator={false}
           data={messages}
           renderItem={({ item }) => {
@@ -91,9 +97,9 @@ export default function Conversation() {
       </View>
 
       <View style={styles.inputView}>
-            <TextInput style={styles.input} 
-            placeholder="Escreva algo" onChangeText={setNewMessage} value={newMessage}></TextInput>
-            <Ionicons style={styles.button} name="paper-plane-outline" size={26} onPress={sendMessage}></Ionicons>
+        <TextInput style={styles.input}
+          placeholder="Escreva algo" onChangeText={setNewMessage} value={newMessage}></TextInput>
+        <Ionicons style={styles.button} name="paper-plane-outline" size={26} onPress={sendMessage}></Ionicons>
       </View>
 
     </KeyboardAvoidingView>
@@ -104,19 +110,19 @@ export default function Conversation() {
 const styles = StyleSheet.create({
 
 
-  messages:{
+  messages: {
     padding: 25,
     paddingTop: 5,
     paddingBottom: 1,
     maxHeight: Dimensions.get('window').height - 180
-  },  
-  inputView:{
+  },
+  inputView: {
     justifyContent: 'center',
   },
-  input:{
+  input: {
     height: 55
   },
-  button:{
+  button: {
 
     position: 'absolute',
     right: 10,
