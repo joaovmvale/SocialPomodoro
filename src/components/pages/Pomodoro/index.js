6f32a5d39f52854fb53a57fb5afcc0b8483838b3
joaovmvale <joao.marquesvale@gmail.com>
2021-11-11 03:33:49 -0300

    Merge com branch pomodoro

--- src/components/pages/Pomodoro/index.js
+++ src/components/pages/Pomodoro/index.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import { Button } from "react-native-paper";
 
-import BackgroundTimer from 'react-native-background-timer';
 import Slider from "@react-native-community/slider";
 import ProgressCircle from "react-native-progress-circle";
 
@@ -41,7 +40,6 @@ export default function Pomodoro() {
   function startTimer() {
     setIsRunning(!isRunning);
     setinitalMinutes(minutes);
-    BackgroundTimer.start();
   }
 
   function resetTimer() {
