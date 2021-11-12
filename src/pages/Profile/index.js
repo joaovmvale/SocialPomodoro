725ac3b213bc1717a0a518fef173aceb457eb691
joaovmvale <joao.marquesvale@gmail.com>
2021-11-11 22:58:14 -0300

    Arrumando perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,21 @@
 import React, { useState, useContext } from "react";
-
-import { View, Text, Alert } from "react-native";
-
-import { Button, TextInput } from "react-native-paper";
+import { View, Text, Alert, Image } from "react-native";
+import { Button, Avatar } from "react-native-paper";
+import AuthContext from "../../contexts/auth";
 
 import firebase from "../../utils/Firebase";
 
+import Styles from "./style";
+
 export default function Profile() {
-  const [description, setDescription] = useState("");
+  const { user } = useContext(AuthContext);
+  const [avatar, setAvatar] = useState("https://firebasestorage.googleapis.com/v0/b/socialpomodoro-b18de.appspot.com/o/defaultprofile.png?alt=media&token=8292a6e7-7c5f-4295-bcf0-c083e3f4611e");
+
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      setAvatar(user.photoURL);
+    }
+  });
 
   async function handleLogout() {
     try {
@@ -17,18 +25,17 @@ export default function Profile() {
     }
   }
 
-  async function handlePost() {
-
-  }
-
   return (
-    <View>
-      <Text>Home</Text>
-      <TextInput
-        value={description}
-        onChangeText={(text) => setDescription(text)}
+    <View style={Styles.container}>
+      <Avatar.Image
+        style={Styles.profileAvatar}
+        size={50}
+        source={
+          avatar
+        }
       />
-      <Button onPress={handleLogout}>LOGOUT</Button>
+      <Text>{user.displayName}</Text>
+      <Button mode="contained" onPress={handleLogout}>LOGOUT</Button>
     </View>
   );
 }
